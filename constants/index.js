92b0fa13719fb5ae169b93ddbacea8cafad80e03
Estiven Max <105964147+e3tiven-max@users.noreply.github.com>
2023-07-23 16:49:45 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8eabcea7b3d3226772d8069c65ea772c4c2ff5e3";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
